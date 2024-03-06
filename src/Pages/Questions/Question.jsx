44da95738dc3a99cd4fe5f49932b9de6b0fe5d44
krgarav <krgarav@gmail.com>
2024-03-06 18:03:46 +0530

    ui improved

--- src/Pages/Questions/Question.jsx
+++ src/Pages/Questions/Question.jsx
@@ -1,14 +1,17 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import classes from "./Question.module.css";
 import QuestionContent from "../../Component/QuestionContent/QuestionContent";
 import Timer from "../../Component/Timer/Timer";
 import { useNavigate } from "react-router";
 import Userinfo from "../../Component/Userinfo/Userinfo";
 import AttemptedSection from "../../Component/AttemptedSection/AttemptedSection";
+import QuestionContext from "../../Store/Questions_context";
 function Question() {
+  const contextStore = useContext(QuestionContext);
   const navigate = useNavigate();
   const submitHanldler = () => {
     alert("Are you sure you want to submit the exam?");
+    console.log(contextStore.answeredQuestion);
     navigate("/", { replace: true });
   };
   return (
